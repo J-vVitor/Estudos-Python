661a8d31a76673e4b8e395bb37041cfac8bd8898
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2025-03-07 19:13:30 -0300

    Adicionais

---
+++ faculdade/aula_01.py
@@ -0,0 +1,6 @@
+# FUNÇÕES COM PYTHON
+
+def soma(x,y):
+    return x + y
+
+print(soma(1,4))
---
+++ faculdade/aula_02.py
@@ -0,0 +1,8 @@
+# CALCULAR A MÉDIA DOS ALUNOS
+
+media_aluno = 24
+
+def notas(x,y):
+    return x + y
+
+print(notas(24,24))
