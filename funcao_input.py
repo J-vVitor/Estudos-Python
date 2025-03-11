bd0041e01f950ada7da8ba06804d872c38d6037d
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2025-03-11 20:28:50 -0300

    Função: Input

---
+++ funcao_input.py
@@ -0,0 +1,20 @@
+# A FUNÇÃO INPUT COLETA DADOS DO USUÁRIO
+
+nome = input('Qual o seu nome: ')
+
+print(nome)
+print(f'Seu nome é {nome=}') 
+
+# NOME_USUARIO PUXA O NOME DA VARIÁVEL E O VALOR DA VARIÁVEL
+# NESSE CASO, "nome_usuario = nome digitado pelo usuário "
+
+
+print('-------------')
+
+
+num_um = int(input('Digite um número: '))
+num_dois = int(input('Digite outro número: '))
+
+soma_dos_numeros = num_um + num_dois
+
+print(f'O número {num_um} + {num_um} = {soma_dos_numeros}')
