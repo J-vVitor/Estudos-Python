aef53fb84ccbb7f4f6583e1787339d7c29de69cd
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2025-03-13 16:41:18 -0300

    Condições

---
+++ condicoes_if,else,elif.py
@@ -0,0 +1,11 @@
+entrar_no_sistema = input('Você quer entrar ou sair do sistema: ')
+
+
+if entrar_no_sistema == 'entrar':
+    print('Você entrou no sistema')    
+    
+elif entrar_no_sistema =='Entrar':
+    print('Você entrou no sistema')  
+    
+else:
+    print('Sair')
