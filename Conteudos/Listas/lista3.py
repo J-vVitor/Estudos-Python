927d4306c254e1a5b40f7b00e830211912b5002b
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2025-05-27 21:32:16 -0300

    Exercício sobre listas

---
+++ Conteudos/Listas/lista3.py
@@ -0,0 +1,18 @@
+# ITERAÇÃO COM LISTAS
+
+
+'''
+    lista_a = ["João","Vitor","Pereira","Lima","Davina"];
+
+    for indice,nome in enumerate(lista_a):
+    print(f'{indice} - {nome}')
+'''
+
+lista_a = ["João","Vitor","Pereira","Lima"];
+contagem_lista = 0;
+
+for indice in lista_a:
+    
+    print(contagem_lista,indice)
+    contagem_lista += 1
+    
