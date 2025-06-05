d10dc168a0c6deef65608194dda0eb77b655a215
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2025-06-04 21:15:28 -0300

    Inicio da atividade

--- Conteudos/Listas/exercicio.py
+++ Conteudos/Listas/exercicio.py
@@ -1,18 +1,30 @@
-""" Lista de compras """
+import os
 
+lista = []
 
+while True:
 
-    while opcao != 'p':
+    print('Selecione uma opção: ')
+    opcoes = input('[i]nserir [a]pagar [l]istar : ')
 
-    print('Selecione uma opção: ');
-    lista=[];
-    opcao = input(f'[i]nserir [a]pagar [l]istar :');
+    if opcoes == 'i':
 
-    if opcao == 'i':
-    input('Item para adicionar: ')
-    lista.append()
-    if opcao == 'l':
-    print(list)
+        item_adicionado_na_lista = input('Item a ser adicionado: ')
 
-    if opcao == 'a':
-    list.pop()
+        lista.append(item_adicionado_na_lista)
+
+        os.system('cls')
+
+    elif opcoes == 'a':
+        print('teste')
+        os.system('cls')
+
+    elif opcoes == 'l':
+        os.system('cls')
+        contagem = 0
+
+        for indice, itens in enumerate(lista):
+
+            print(indice, itens)
+            contagem += 1
+        break
