bd02f3eeb93507041d5a7c90adceb16f36a2d1d9
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2025-06-13 22:10:00 -0300

    Validador de CPF

--- Conteudos/operador ternario/ex2.py
+++ Conteudos/operador ternario/ex2.py
@@ -2,4 +2,4 @@ numero_recbido = int(input('Digite um valor: '))
 
 condicao = numero_recbido if numero_recbido <= 9 else 'maior que 10'
 
-print(condicao)
+print(condicao)8
---
+++ tarefas/validador de CPF/validador.py
@@ -0,0 +1,61 @@
+numero_um = int(input('Digite um numero: '))
+num_um = numero_um * 10
+resultado_num_um = num_um
+
+print(resultado_num_um)
+
+numero_dois = int(input('Digite um numero: '))
+num_dois = numero_dois * 9
+resultado_num_dois = num_dois
+
+print(resultado_num_dois)
+
+numero_tres = int(input('Digite um numero: '))
+num_tres = numero_tres * 8
+resultado_num_tres = num_tres
+
+print(resultado_num_tres)
+
+numero_quatro = int(input('Digite um número: '))
+num_quatro = numero_quatro * 7
+resultado_num_quatro = num_quatro
+
+print(resultado_num_quatro)
+
+numero_cinco = int(input('Digite um número: '))
+num_cinco = numero_cinco * 6
+resultado_num_cinco = num_cinco
+
+print(resultado_num_cinco)
+
+numero_seis = int(input('Digite um número: '))
+num_seis = numero_seis * 5
+resultado_num_seis = num_seis
+
+print(resultado_num_seis)
+
+numero_sete = int(input('Digite um número: '))
+num_sete = numero_sete * 4
+resultado_num_sete = num_sete
+
+print(resultado_num_sete)
+
+numero_oito = int(input('Digite um número: '))
+num_oito = numero_oito * 3
+resultado_num_oito = num_oito
+
+print(resultado_num_oito)
+
+numero_nove = int(input('Digite um número: '))
+num_nove = numero_nove * 2
+resultado_num_nove = num_nove
+
+print(resultado_num_nove)
+
+# SOMA DE TODOS OS NUMEROS
+
+soma_de_todos_os_numeros = resultado_num_um+resultado_num_dois+resultado_num_tres+resultado_num_quatro + \
+    resultado_num_cinco+resultado_num_seis + \
+    resultado_num_sete+resultado_num_oito+resultado_num_nove
+
+print(soma_de_todos_os_numeros)
