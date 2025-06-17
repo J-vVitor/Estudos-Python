aca8ba3cd3932a803c471f9fc2452851a4949914
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2025-06-16 21:40:16 -0300

    Incluindo um novo método.

--- tarefas/validador de CPF/teste_validador.py
+++ tarefas/validador de CPF/teste_validador.py
@@ -1,1 +1,18 @@
-TENTAR NOVAMENTE!!
+receber_cpf = input('Digite o CPF: ')
+contador = 10
+
+primeiro_digito = 0
+
+for numeros in receber_cpf:
+
+    primeiro_digito += int(numeros) * contador
+
+    contador -= 1
+
+
+# VALOR DOS NÚMEROS DO CPF MULTIPLICADO POR 10 E DIVIDIDO POR 11, COM O RESTO DA DIVISÃO
+valor_primeiro_digito = ((primeiro_digito * 10) % 11)
+
+
+# VALOR DO CPF COM O PRIMEIRO DIGITO INCLUSO.
+concatecao_com_primero_digito = (receber_cpf + str(valor_primeiro_digito))
