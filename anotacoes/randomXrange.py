051a9719d2e40d83046bb39215d8ccb5b1d29df6
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2025-06-29 16:33:22 -0300

    Alterações

---
+++ anotacoes/randomXrange.py
@@ -0,0 +1,16 @@
+""""
+
+    Range - Lista um seuquência númerica definida.
+    
+    Exemplo:
+    
+        range(9) - " 0,1,2,3,4,5,6,7,8,9 "
+
+
+    Random - Exibe um número aleatória de uma sequeência definida.
+    
+    Exemplo:
+    
+        random.randint(0,9) - " 6 "
+
+"""
--- tarefas/string_repetida.py
+++ tarefas/string_repetida.py
@@ -1,8 +1,13 @@
 import random
+import sys
 
 cpf = ''
 
 for i in range(9):
-    cpf += str(random.randint(0, 8))
+    cpf += str(random.randint(0, 9))
 
 print(cpf)
+
+sys.exit()
+
+print('teste')
