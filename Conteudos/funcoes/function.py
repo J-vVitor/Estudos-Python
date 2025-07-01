02a65946e395cd3549fad44d4e52957a0efc17c5
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2025-06-30 21:39:31 -0300

    Inicio function

---
+++ Conteudos/funcoes/function.py
@@ -0,0 +1,17 @@
+
+def soma(x, y):
+
+    print(f'{x = } + {y = } = {x+y}')
+
+
+soma(8, 4)
+
+# ----------------------------------------
+
+
+def soma_dois(x, y):
+
+    print(f'{x = } + {y = } = {x+y}')
+
+
+soma_dois(x=4, y=10)
--- tarefas/calculadora while.py
+++ tarefas/calculadora while.py
@@ -3,39 +3,37 @@
 
 '''
 while True:
-    
+
     # CAMPO PARA RECEBER INFORMAÇÕES DO USUÁRIO
-    num_um = input('Digite um número: ');
-    num_dois = input('Digite outro número: ');
-    operador = input('Digite o operador ( + - * / ) ');
-    
+    num_um = input('Digite um número: ')
+    num_dois = input('Digite outro número: ')
+    operador = input('Digite o operador ( + - * / ) ')
+
     # CAMPO PARA CONVERSÃO DE STRING PARA INTEIRO
     num_um_conversao = int(num_um)
     num_dois_conversao = int(num_dois)
-    
+
     # CAMPO QUE VERIFICA SE FORAM DIGITADOS NÚMEROS
     if num_um.isdigit() and num_dois.isdigit():
-    
+
         if operador == '+':
-            print(num_um_conversao + num_dois_conversao);
-            
+            print(num_um_conversao + num_dois_conversao)
+
         elif operador == '-':
-            print(num_um_conversao - num_dois_conversao);   
-            
+            print(num_um_conversao - num_dois_conversao)
+
         elif operador == '*':
-            print(num_um_conversao * num_um_conversao);
-            
+            print(num_um_conversao * num_um_conversao)
+
         elif operador == '/':
-            print(num_um_conversao // num_dois_conversao);
-            
+            print(num_um_conversao // num_dois_conversao)
+
         else:
             print('Você não digitou um operador')
-             
+
     continuacao = input('Deseja continuar? [s]im [n]ão ')
-    
+
     if continuacao == 's' or continuacao == 'S':
         continue
     else:
         break
-            
-        
