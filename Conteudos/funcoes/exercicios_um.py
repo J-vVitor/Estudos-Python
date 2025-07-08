0d479980763594fc7939c17daef08b45a1b27a6e
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2025-07-08 20:14:39 -0300

    Update exercicios_um.py

--- Conteudos/funcoes/exercicios_um.py
+++ Conteudos/funcoes/exercicios_um.py
@@ -1,2 +1,25 @@
 # crie um funçãp que multiplica todos os argumento não nomeados recebidos
 # Retorne o total oara uma variavel e mostre o valor da variavel
+
+def multiplicar(*args):
+    resultado = 1
+
+    for numeros in args:
+        resultado *= numeros
+
+    return resultado
+
+
+multilplicacao = multiplicar(4, 5)
+
+print(multilplicacao)
+
+# PAR OU ÍMPAR
+valor = int(input('Digite um número: '))
+
+if valor % 10 == 0:
+    print('Número par!')
+
+
+else:
+    print('Número ímpar')
