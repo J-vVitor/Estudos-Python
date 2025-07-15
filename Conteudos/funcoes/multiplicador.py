db22c95c0f8bc9d733faf37ebd993a4426dbf848
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2025-07-15 20:02:34 -0300

    Update multiplicador.py

--- Conteudos/funcoes/multiplicador.py
+++ Conteudos/funcoes/multiplicador.py
@@ -1,12 +1,2 @@
 # CRIAR UMA FUNÇÃO QUE MULTIPLICA, TRIPLICA E QUADRUPLICA O
 # PARÂMETRO RECEBIDO
-
-def multiplicacao(numero):
-
-    def multiplicar(multiplicador):
-        return numero * multiplicador
-
-    return multiplicacao()
-
-
-print(multiplicacao(4))
