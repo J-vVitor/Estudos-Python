cc27878340bbe66bef1c187761b9e9557e0980bc
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2025-08-19 21:06:51 -0300

    Inicio da função lambda

---
+++ Conteudos/funcoes/function_lambda/ex01.py
@@ -0,0 +1,9 @@
+# Inicio com lambda
+
+# MODO DE USAR
+
+
+def somar(x, y): return x+y
+
+
+print(somar(2, 2))
---
+++ Conteudos/funcoes/function_lambda/ex02.py
@@ -0,0 +1,7 @@
+lista = [
+    {'nome': 'João', 'sobrenome': 'Vitor'},
+    {'nome': 'Thiago', 'sobrenome': 'Bruno'},
+    {'nome': 'Bruno', 'sobrenome': 'Da silva'},
+    {'nome': 'Cesar ', 'sobrenome': 'Luiz'},
+    {'nome': 'Felipe', 'sobrenome': 'Tavares'},
+]
